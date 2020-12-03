b34cc137ebba3c6879134ecd069de1ffa77d0d81
Chris Lusty <C.Lusty@me.com>
2020-12-03 23:15:07 +0000

    Added detection when a player gets 3 in a row, with winner message and early loop break.

--- 0xgame.py
+++ 0xgame.py
@@ -1,4 +1,4 @@
-import random
+import random # used later to make a random decision on which player starts
 
 # function to take a user input, and check that it is one of the permitted options
 def takeInput(inputOptions, inputPrompt):
@@ -28,7 +28,22 @@ allSpaces = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 
 # define the locations where we can later put text on the board
 spaceLocations = [51, 59, 67, 142, 150, 158, 233, 241, 249]
+
+# define the various combinations of locations that equate to a win (three in a row)
+winningCombos = [{"A1", "A2", "A3"}, {"B1", "B2", "B3"}, {"C1", "C2", "C3"}, {"A1", "B1", "C1"}, {"A2", "B2", "C2"}, {"A3", "B3", "C3"}, {"A1", "B2", "C3"}, {"A3", "B2", "C1"}]
+
 #=====================================================
+
+print("""
+*********************************************************
+Welcome to Noughts and Crosses.
+
+This is a two player game. Players must assign themselves
+as either noughts (0) or crosses (X).
+
+Please follow the on screen prompts to play. Have fun!
+*********************************************************
+""")
 
 # ask who gets to have the first go, noughts or crosses
 firstGoOptions = ["0","X","r"]
@@ -40,15 +55,20 @@ toStart = takeInput(firstGoOptions, pickFirstGoPrompt)
 # make random choice of start player if required
 if toStart == "r":
   toStart = random.choice(["X","0"])
-  print("Random choice: player \"{}\" starts.".format(toStart))
+  print("\n Random choice: player \"{}\" starts.".format(toStart))
 
 currentTurn = toStart
 
 print(blankBoard)
 
-availableSpaces = allSpaces.copy() # weirdly, if you set list 2 = list 1, changes made to list 2 are reflected in list 1. Therefore use copy() to make a separate list.
+availableSpaces = allSpaces.copy() # weirdly, if you set list2 = list1, changes made to list2 are reflected in list1. Therefore use copy() to make list2 a separate list.
+
+ownedByX = set() # create blank set to store locations owned by X
+ownedBy0 = set() # create blank set to store locations owned by 0
 
 workingBoard = blankBoard
+
+timeToEnd = False
 
 for turns in range(9):
 
@@ -70,27 +90,43 @@ for turns in range(9):
 	print(workingBoard)
 	
 	if currentTurn == "X":
-	  currentTurn = "0"
+		
+		ownedByX.add(requestedSpace)
+		currentTurn = "0"
+		
+		for i in range(len(winningCombos)):
+			if winningCombos[i].issubset(ownedByX) == True:
+				print("""
+	********************
+	********************
+	***    X wins!   ***
+	********************
+	********************
+				""")
+				timeToEnd = True
+				break
 	else:
-	  currentTurn = "X"
+		
+		ownedBy0.add(requestedSpace)
+		currentTurn = "X"
+		
+		for i in range(len(winningCombos)):
+			if winningCombos[i].issubset(ownedBy0) == True:
+				print("""
+	********************
+	********************
+	***    0 wins!   ***
+	********************
+	********************
+				""")
+				timeToEnd = True
+				break
+	
+	if timeToEnd == True:
+		break
+	
+	
+	
+	
 
-#print(currentTurn)
-
-
-
-#====================================================3
-
-#for m in range(len(output)):
- # print(m, output[m])
-  
-"""
-A1 = 51
-A2 = 59
-A3 = 67
-B1 = 142
-B2 = 150
-B3 = 158
-C1 = 233
-C2 = 241
-C3 = 249
-"""
+#====================================================
