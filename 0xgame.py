519ce5c5d758ffdc9d3fb28a3ea6a9fc627a14b8
Chris Lusty <C.Lusty@me.com>
2020-12-03 16:26:13 +0000

    Got user input up and running with error trap

--- 0xgame.py
+++ 0xgame.py
@@ -1,4 +1,23 @@
-toStart = input("Who goes first? (Enter 0, X, or r for random): ")
+import random
+
+# function to take a user input, and check that it is one of the permitted options
+def takeInput(inputOptions, inputPrompt):
+  valEntered = ""
+  while valEntered not in inputOptions:
+  	 valEntered = input(inputPrompt)
+  return valEntered
+
+# ask who gets to have the first go, noughts or crosses
+firstGoOptions = ["0","X","r"]
+
+pickFirstGoPrompt = "Who goes first? (Enter {}, {} or {} for random): ".format("0","X","r")
+  
+toStart = takeInput(firstGoOptions, pickFirstGoPrompt)
+
+# make random choice of start player if required
+if toStart == "r":
+  toStart = random.choice(["X","0"])
+  print("Random choice: player \"{}\" starts.".format(toStart))
 
 currentTurn = toStart
 
@@ -14,16 +33,31 @@ print(currentTurn)
 output = """
       1       2       3
           |       |
-A         |       |
+A         |       |    
           |       |
    -------|-------|-------
           |       |
-B         |       |
+B         |       |    
           |       |
    -------|-------|-------
           |       |
-C         |   X   |   0
+C         |       |    
           |       |
 """
 
 print(output)
+
+#for m in range(len(output)):
+ # print(m, output[m])
+  
+"""
+A1 = 51
+A2 = 59
+A3 = 67
+B1 = 142
+B2 = 150
+B3 = 158
+C1 = 233
+C2 = 241
+C3 = 249
+"""
